ce8692c0f3d0628b86cb97c7cc25484ba7052d19
Utero93 <nirvanautero93@gmail.com>
2024-02-03 14:26:14 -0500

    new changes and created helper functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,40 @@
 // imported modules and packages
 const apiRoute = require("express").Router();
 const path = require("path");
-const fs = require("fs");
-
 // middleware for generating unique ids
 const { v4: uuidv4 } = require("uuid");
 
+const fs = require("fs");
+
+//  stores the path to the JSON data file 
+const dbPath = 'db/db.json';
+
+const readJson = () => {
+
+  const data = fs.readFileSync(dbPath, 'utf-8');
+  return JSON.parse(data);
+};
+
+const writeJson = (data) => {
+fs.writeFileSync(dbPath, JSON.stringify(data));
+
+};
+
 // sends the db.json to the path /api/notes
 apiRoute.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "./db/db.json"));
+const data = readJson();
+res.json(data);
 });
+
+// apiRoute.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "./db/db.json"));
+// });
+
 
 // adds user notes to the db.json
 apiRoute.post("/notes", (req, res) => {
   // read db.json and parses the JSON object // use readFileSynce because it runs synchronously instead of async (prevents promise errors)
-  let data = JSON.parse(fs.readFileSync("db/db.json"));
+  let data = readJson();
   //   creates an object based on user notes and title, then gives it a unique id
   let newNote = {
     title: req.body.title,
@@ -24,21 +44,23 @@ apiRoute.post("/notes", (req, res) => {
   //   adds the new note to the data array of objects in db.json
   data.push(newNote);
   //   writes the file all over with pre-existing and new data
-  fs.writeFileSync("db/db.json", JSON.stringify(data));
+  writeJson(data);
   //   res.json is sending the array of objects with the new data(notes)
   res.json(data);
 });
 
+
+
 // deletes user notes by unique id values
 apiRoute.delete("/notes/:id", (req, res) => {
   // reads the database and stores the array into the 'data' variable
-  let data = JSON.parse(fs.readFileSync("db/db.json"));
+  let data = readJson();
   // filters out all the notes that dont equal the specified id
-  data = data.filter((note) => note.id !== req.params.id);
+  const deleteNote = data.filter((note) => note.id !== req.params.id);
   // writes the array to the db.json
-  fs.writeFileSync("db/db.json", JSON.stringify(data));
+ writeJson(deleteNote);
   //   res.json is sending the array of objects with the new data(notes)
-  res.json(data);
+  res.json({message: 'Note has been deleted!'});
 });
 
 module.exports = apiRoute;
